74381b0454919a9f57aa801f646c3eaa4c3c97b7
Beeriand <adiche@wp.pl>
2020-04-20 22:44:44 +0200

    Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject btnSzalka1;
+    public GameObject btnSzalka2;
+    public Image menu;
+    void Start()
+    {
+        btnSzalka1 = GameObject.Find("Button");
+        btnSzalka2 = GameObject.Find("Button2");
+        menu = menu.GetComponent<Image>();
+
+        btnSzalka2.SetActive(false);
+    }
+
+    public void BTNSzczalka()
+    {
+
+         menu.transform.localPosition = new Vector3(menu.transform.localPosition.x + 500, menu.transform.localPosition.y, menu.transform.localPosition.z);
+         btnSzalka1.SetActive(false);
+         btnSzalka2.SetActive(true);
+
+    }   
+    public void BTNSzczalka2()
+    {
+
+         menu.transform.localPosition = new Vector3(menu.transform.localPosition.x - 500, menu.transform.localPosition.y, menu.transform.localPosition.z);
+         btnSzalka2.SetActive(false);
+         btnSzalka1.SetActive(true);
+
+    }
+}
